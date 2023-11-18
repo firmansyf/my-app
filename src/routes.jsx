ef538b7aac4bfc75ccf0151825a936d76f69dcd9
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2023-11-18 09:16:27 +0700

    initialize--app

---
+++ src/routes.jsx
@@ -0,0 +1,58 @@
+import {
+  HomeIcon,
+  TableCellsIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+     
+      {
+        icon: <TableCellsIcon {...icon} />,
+        name: "member",
+        path: "/tables",
+        element: <Tables />,
+      },
+      // {
+      //   icon: <InformationCircleIcon {...icon} />,
+      //   name: "notifications",
+      //   path: "/notifications",
+      //   element: <Notifications />,
+      // },
+    ],
+  },
+
+
+  // {
+  //   title: "auth pages",
+  //   layout: "auth",
+  //   pages: [
+  //     {
+  //       icon: <ServerStackIcon {...icon} />,
+  //       name: "sign in",
+  //       path: "/sign-in",
+  //       element: <SignIn />,
+  //     },
+  //     {
+  //       icon: <RectangleStackIcon {...icon} />,
+  //       name: "sign up",
+  //       path: "/sign-up",
+  //       element: <SignUp />,
+  //     },
+  //   ],
+  // },
+];
+
+export default routes;

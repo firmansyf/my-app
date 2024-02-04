866fddf620eaa8d9cd7ac8d6747b3f5597391f8e
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-02-04 11:29:23 +0700

    wip: project-sticky-notes

--- src/pages/project/sticky-notes/index.jsx
+++ src/pages/project/sticky-notes/index.jsx
@@ -1,8 +1,15 @@
-import {Button, Card, CardBody, Input, Typography} from '@material-tailwind/react'
+import {Button} from '@material-tailwind/react'
 import React, {useState} from 'react'
+import './style.css'
+import ModalNote from './ModalNote'
 
 export default function StickyNotes() {
-  const [data, setData] = useState([])
+  // const [position, setPosition] = useState({x: 0, y: 0})
+  const [showModal, setShowModal] = useState(false)
+  const [data, setData] = useState([
+    {name: 'Sample 1', priority: 'LOW'},
+    {name: 'Sample 2', priority: 'Medium'},
+  ])
   const [notes, setNotes] = useState({
     name: '',
     priority: '',
@@ -26,49 +33,33 @@ export default function StickyNotes() {
 
     setData(result)
     setNotes({name: '', priority: ''})
+    setShowModal(false)
   }
 
   return (
-    <main className='h-screen bg-amber-100 p-9'>
-      <Card className='card w-1/4'>
-        <CardBody className='flex flex-col gap-3 p-4'>
-          <Typography>Sticky Notes</Typography>
-          <div className='w-full'>
-            <Input
-              placeholder='Enter your item'
-              className='outline-none'
-              name='name'
-              value={notes.name}
-              onChange={(e) => handleOnChange(e)}
-            />
-          </div>
-          <div className='w-full'>
-            <select
-              label='Select Priority'
-              name='priority'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-              value={notes.priorty}
-              onChange={(e) => handleOnChange(e)}
-            >
-              <option value=''>Choose Priority</option>
-              <option value='low'>Low</option>
-              <option value='medium'>Medium</option>
-              <option value='highest'>Highest</option>
-            </select>
-          </div>
-          <div>
-            <Button size='sm' onClick={handleOnSubmit}>
-              Submit
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-      <div className='content'>
-        {Array.isArray(data) &&
-          data?.map((item, i) => {
-            return <></>
-          })}
-      </div>
-    </main>
+    <>
+      <main className='h-screen bg-amber-100 p-9 '>
+        <Button onClick={() => setShowModal(true)}>Write Note</Button>
+
+        <div className='content bg-red-500'>
+          {Array.isArray(data) &&
+            data?.map((item, index) => {
+              return (
+                <div draggable key={index} className='section-content cursor-move shadow-md'>
+                  <span className='text-white'>{item?.name}</span>
+                </div>
+              )
+            })}
+        </div>
+      </main>
+
+      <ModalNote
+        notes={notes}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        handleOnClick={handleOnSubmit}
+        handleOnChange={handleOnChange}
+      />
+    </>
   )
 }

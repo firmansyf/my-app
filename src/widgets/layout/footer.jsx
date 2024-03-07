3e8df4c5b750afdca0855b2b13a400ad68b8ce7a
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-03-07 18:51:29 +0700

    feat: improve--section-experience-and-footer

--- src/widgets/layout/footer.jsx
+++ src/widgets/layout/footer.jsx
@@ -8,7 +8,7 @@ export function Footer({routes}) {
     <footer className='py-5 mt-7'>
       <div className='flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between'>
         <Typography variant='small' className='font-normal text-inherit'>
-          &copy; {year} yusuf Firmansyah
+          &copy; {year} Yusuf Firmansyah
         </Typography>
         <ul className='flex items-center gap-4'>
           {routes.map(({name, path}) => (
